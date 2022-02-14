f13dd8d74a6fa5ca5d2519c76db9ba5b2b62a6be
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-02-14 09:26:41 +0300

    'initial commit'

---
+++ WorkingWithSasha/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace WorkingWithSasha;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var microBus = new Bus(90, false, 2);
+        Console.WriteLine(microBus);
+        if (!microBus.IsPublic) microBus.IsPublic = true;
+        microBus.Drive(133);
+        Console.WriteLine(microBus);
+    }
+}
+internal abstract class Vehicle
+{
+    protected VehicleType Type { get; }
+    protected int MaxSpeed { get; }
+    protected internal bool IsPublic { get; set; }
+    protected int Kilometerage { get; set; }
+    protected Vehicle(VehicleType vehicleType, int maxSpeed, bool isPublic)
+    {
+        Type = vehicleType;
+        MaxSpeed = maxSpeed;
+        IsPublic = isPublic;
+        Kilometerage = 0;
+    }
+    public override string ToString()
+    {
+        return "Vehicle type: " + Type + ". Max speed: " + MaxSpeed + ", belongs to public transport: " + IsPublic + 
+            ", and it passed " + Kilometerage + ".";
+    }
+
+}
+
+internal class Bus: Vehicle
+{
+    private int NumOfDoors { get; }
+    public Bus(int maxSpeed, bool isPublic, int numberOfDoors) : base(VehicleType.Bus, maxSpeed, isPublic)
+    {
+        NumOfDoors = numberOfDoors;
+    }
+
+    public void Drive(int numOfKilometres)
+    {
+        Kilometerage += numOfKilometres;
+    }
+    
+    public override string ToString()
+    {
+        return "Vehicle type: " + Type + ". Max speed: " + MaxSpeed + ", belongs to public transport: " + IsPublic + 
+               ", and it passed " + Kilometerage + ". It has " + NumOfDoors + " brilliant doors.";
+    }
+}
+internal enum VehicleType
+{
+    Bus, 
+    Trolleybus,
+    Tram,
+    Coach,
+    Car,
+    Train,
+    Plane,
+    Spaceship
+}
